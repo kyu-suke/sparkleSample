2d965b77f23a2d1b860f013497c8b29e68b22f53
kyu-suke <ykaneoka7@gmail.com>
2019-01-06 16:55:42 +0900

    1.0

---
+++ sparkleSample/Pods/Target Support Files/Pods-sparkleSample/Pods-sparkleSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sparkleSample : NSObject
+@end
+@implementation PodsDummy_Pods_sparkleSample
+@end
---
+++ sparkleSample/sparkleSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sparkleSample
+//
+//  Created by kyusuke on 2019/01/06.
+//  Copyright © 2019 kyusuke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
